6ca1595efc6570588aa4e9e26148ae17257c87e9
Ruth <ruthjohnson2013@gmail.com>
2016-04-25 18:43:57 -0700

    fixed multiple annotation plotting

--- CANVAS.py
+++ CANVAS.py
@@ -9,9 +9,32 @@ from scipy.stats import norm
 import math
 from optparse import OptionParser
 import svgutils.transform as sg
-#import cairosvg
+import cairosvg
 import sys
 
+
+def vararg_callback(option, opt_str, value, parser):
+    assert value is None
+    value = []
+
+    def floatable(str):
+        try:
+            float(str)
+            return True
+        except ValueError:
+            return False
+
+    for arg in parser.rargs:
+        # stop on --foo like options
+        if arg[:2] == "--" and len(arg) > 2:
+            break
+        # stop on -a, but not on -3 or -3.0
+        if arg[:1] == "-" and len(arg) > 1 and not floatable(arg):
+            break
+        value.append(arg)
+
+    del parser.rargs[:len(value)]
+    setattr(parser.values, option.dest, value)
 
 def Read_Input(locus_fname, ld_fname, annotation_fname):
     """Function that reads in all your data """
@@ -20,11 +43,14 @@ def Read_Input(locus_fname, ld_fname, annotation_fname):
     locus_data = [row[:] for row in csv_file]
     locus = np.array(locus_data, dtype='double')
     ld = pd.read_csv(ld_fname, header=None, delimiter=r"\s+")
-    csv_file = csv.reader(open(annotation_fname, 'rb'), delimiter=' ')
-    file_header = csv_file.next()
-    annotation_data = [row[:] for row in csv_file]
-    annotation = np.array(annotation_data, dtype='int_')
-    annotation = annotation[:, 1]
+    annotation = []
+    for i in range(0, len(annotation_fname)):
+        csv_file = csv.reader(open(annotation_fname[i], 'rb'), delimiter=' ')
+        file_header = csv_file.next()
+        annotation_data = [row[:] for row in csv_file]
+        annotation_array = np.array(annotation_data, dtype='int_')
+        annotation_array = annotation_array[:, 1]
+        annotation.append(annotation_array)
     return [locus, ld, annotation]
 
 def Plot_Position_Value(position, zscore, pos_prob ):
@@ -35,13 +61,13 @@ def Plot_Position_Value(position, zscore, pos_prob ):
     plt.xlim(np.amin(position), np.amax(position))
     plt.ylabel('-log10(pvalue)')
     pvalue = Zscore_to_Pvalue(zscore)
-    sub1.scatter(position, pvalue)
+    sub1.scatter(position, pvalue, color='#D64541')
     sub2 = fig.add_subplot(2, 1, 2, axisbg='white')
     plt.xlim(np.amin(position), np.amax(position))
     plt.gca().set_ylim(bottom=0)
     plt.ylabel('Posterior probabilities')
     plt.xlabel('Location')
-    sub2.scatter(position, pos_prob)
+    sub2.scatter(position, pos_prob, color='#2980b9')
     value_plots = fig
     return value_plots #returns subplots with both graphs
 
@@ -60,72 +86,61 @@ def Plot_Heatmap(correlation_matrix, hue1, hue2):
     heatmap = fig
     return heatmap
 
-def Plot_Annotations(annotation_vectors, annotation_names):
+def Plot_Annotations(annotation_names, annotation_vectors):
     """Plot the annotations with labels"""
-    colors = []
-    for a in annotation_vectors:
-        if a == 0:
-            colors.append('#DADFE1')
-        else:
-            colors.append('#4B77BE')
-    fig = plt.figure(figsize=(12, 1.0))
-    ax2 = fig.add_axes([0.05, 0.475, 0.9, 0.15])
-    cmap = mpl.colors.ListedColormap(colors)
-    cmap.set_over('0.25')
-    cmap.set_under('0.75')
-    N = len(annotation_vectors)
-    bounds = range(1, N+1)
-    norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
-    annotation_plot1 = mpl.colorbar.ColorbarBase(ax2, cmap=cmap,
-                                    norm=norm,
-                                    spacing='proportional',
-                                    orientation='horizontal')
-    annotation_plot1.set_label('Annotations')
-    annotation_plot1 = plt
-
-    colors = []
-    for a in annotation_vectors:
-        if a == 0:
-            colors.append('#DADFE1')
-        else:
-            colors.append('#4B77BE')
-    fig = plt.figure(figsize=(12, 1.0))
-    ax2 = fig.add_axes([0.05, 0.475, 0.9, 0.15])
-    cmap = mpl.colors.ListedColormap(colors)
-    cmap.set_over('0.25')
-    cmap.set_under('0.75')
-    N = len(annotation_vectors)
-    bounds = range(1, N + 1)
-    norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
-    annotation_plot2 = mpl.colorbar.ColorbarBase(ax2, cmap=cmap,
-                                                norm=norm,
-                                                spacing='proportional',
-                                                orientation='horizontal')
-    annotation_plo2.set_label('Annotations')
-    annotation_plot2 = plt
-
-    return (annotation_plot1, annotation_plot2)
+    annotation_tuple = []
+    color_array = ['#663399', '#e74c3c', '#049372', '#F89406', '#1E8BC3']
+    for i in range(0, len(annotation_names)):
+        annotation = annotation_vectors[i]
+        colors = []
+        for a in annotation:
+            if a == 1:
+                colors.append(color_array[i])
+            else:
+                colors.append('#ecf0f1')
+        fig = plt.figure(figsize=(12, 1.0))
+        ax2 = fig.add_axes([0.05, 0.475, 0.9, 0.15])
+        cmap = mpl.colors.ListedColormap(colors)
+        cmap.set_over('0.25')
+        cmap.set_under('0.75')
+        n = len(annotation)
+        bounds = range(1, n+1)
+        norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+        annotation_plot = mpl.colorbar.ColorbarBase(ax2, cmap=cmap,
+                                                     norm=norm,
+                                                     spacing='proportional',
+                                                     orientation='horizontal')
+        annotation_plot.set_label(annotation_names[i])
+        annotation_plot = fig
+        annotation_tuple.append(annotation_plot)
+    return annotation_tuple
 
 def Assemble_Figure(value_plots, heatmap, annotation_plot):
     """Assemble everything together"""
     value_plots.savefig('value_plots.svg', format='svg', dpi=1200)
     heatmap.savefig('heatmap.svg', format='svg', dpi=1200)
-    annotation_plot.savefig('annotation_plot.svg', format='svg', dpi=1200)
-    fig = sg.SVGFigure("13in", "19in")
+    fig = sg.SVGFigure("13in", "23in")
     value_plots = sg.fromfile('value_plots.svg')
     heatmap = sg.fromfile('heatmap.svg')
-    annotation_plot = sg.fromfile('annotation_plot.svg')
-
     plot1 = value_plots.getroot()
     plot2 = heatmap.getroot()
-    plot3 = annotation_plot.getroot()
-    plot2.rotate(-45, 600, 600)
-    plot2.moveto(250, 100, scale=1.375)
-    plot3.moveto(60, 825, scale=.9)
+    y_scale = 50*(len(annotation_plot))
+    plot2.moveto(-10, 600 + y_scale, scale=1.425)
+    plot2.rotate(-45, 0, 0)
+    fig.append([plot2, plot1])
 
-    fig.append([plot2, plot1, plot3])
+    index = 0
+    for plot in annotation_plot:
+        plot.savefig('annotation_plot.svg', format='svg', dpi=1200)
+        plot = sg.fromfile('annotation_plot.svg')
+        plot3 = plot.getroot()
+        y_move = 375 + 72*(index+1)
+        plot3.moveto(60, y_move, scale=.9)
+        index += 1
+        fig.append(plot3)
+
     fig.save("fig_final.svg")
-    #cairosvg.svg2pdf(url='fig_final.svg', write_to='fig_final.pdf')
+    cairosvg.svg2pdf(url='fig_final.svg', write_to='fig_final.pdf')
 
 
 def Zscore_to_Pvalue(zscore):
@@ -142,20 +157,20 @@ def main():
     # Parse the command line data
     parser = OptionParser()
     parser.add_option("-l", "--locus_name", dest="locus_name")
-    parser.add_option("-a", "--annotation_name", dest="annotation_name")
+    #parser.add_option("-a", "--annotation_name", dest="annotation_name")
+    parser.add_option("-n", "--number_of_args", )
+    parser.add_option("-a", "--annotation_names", dest="annotation_names", action='callback', callback=vararg_callback)
+    parser.add_option("-p", "--annotation_plot", dest="annotation_plot", action='callback', callback=vararg_callback)
     parser.add_option("-r", "--ld_name", dest="ld_name")
-    parser.add_option("-p", "--plot_annotations", dest="plot_annotations")
-    parser.add_option("--h1", "--hue1", dest="hue1")
-    parser.add_option("--h2", "--hue2", dest="hue2")
-    # add other optinos to parse
+    parser.add_option("--h1", "--hue1", dest="hue1", default=240)
+    parser.add_option("--h2", "--hue2", dest="hue2", default=10)
 
     # extract options
     (options, args) = parser.parse_args()
-
     locus_name = options.locus_name
-    annotation_name = options.annotation_name
     ld_name = options.ld_name
-    plot_annotations = options.plot_annotations
+    annotation_names = options.annotation_names
+    annotation_plot = options.annotation_plot
     hue1 = options.hue1
     hue2 = options.hue2
     usage = \
@@ -173,10 +188,10 @@ def main():
     """if(locus_name == None or annotation_name == None or ld_name == None or plot_annotations == None):
         sys.exit(usage)"""
 
-    [locus, ld, annotation] = Read_Input(locus_name, ld_name, annotation_name)
-    value_plots = Plot_Position_Value(locus[:, 0], locus[:, 1],locus[:, 2])
+    [locus, ld, annotation] = Read_Input(locus_name, ld_name, annotation_plot)
+    value_plots = Plot_Position_Value(locus[:, 0], locus[:, 1], locus[:, 2])
     heatmap = Plot_Heatmap(ld, hue1, hue2)
-    annotation_plot = Plot_Annotations(annotation, annotation_name)
+    annotation_plot = Plot_Annotations(annotation_names, annotation)
 
     Assemble_Figure(value_plots, heatmap, annotation_plot)
 
